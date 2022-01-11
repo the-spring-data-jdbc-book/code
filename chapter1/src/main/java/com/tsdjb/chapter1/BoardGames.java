13733c3ade539a4793efa5bda2923a706a667f05
Jens <schauder>
2022-01-11 23:18:50 +0100

    Entity and Repository are both package private.
    Only mixing public and package private causes problems.

--- chapter1/src/main/java/com/tsdjb/chapter1/BoardGames.java
+++ chapter1/src/main/java/com/tsdjb/chapter1/BoardGames.java
@@ -2,6 +2,6 @@ package com.tsdjb.chapter1;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface BoardGames extends CrudRepository<BoardGame, Long> {
+interface BoardGames extends CrudRepository<BoardGame, Long> {
     BoardGame findByName(String name);
 }
